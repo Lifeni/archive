422da6347bdb11f80105435feeaf75823cfc6507
Lifeni <2072945536@qq.com>
2019-07-10 19:29:36 +0800

    kill bug

---
+++ javascript/main.js
@@ -0,0 +1,20 @@
+window.onload = function () {
+    if (document.getElementById("main")) {
+        var main = document.getElementById("main");
+    }
+}
+
+/*
+// div 随鼠标移动代码
+if (document.getElementById("card")) {
+    main.onmousemove = function () {
+        var card = document.getElementById("card");
+        var moveX = (event.clientX - (document.body.clientWidth / 2)) / 200;
+        var moveY = (event.clientY - (document.body.clientHeight / 2)) / 200;
+
+        card.style.transform = "translate(" + moveX + "px," + moveY + "px)";
+        // console.log(moveX + " " + moveY);
+        // console.log(card.style.transform);
+    }
+}
+*/
