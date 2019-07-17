d9804f555c9db4ee37f81e1de28cd6a2c9f16840
Lifeni <liangfengning@foxmail.com>
2019-07-18 00:29:57 +0800

    增加背景

--- javascript/main.js
+++ javascript/main.js
@@ -1,8 +1,59 @@
-window.onload = function () {
-    if (document.getElementById("main")) {
-        var main = document.getElementById("main");
+let header = document.getElementById("header");
+let main = document.getElementById("main");
+let footer = document.getElementById("footer");
+
+let card = document.getElementsByClassName("card");
+
+let background = document.getElementById("background");
+let backgroundSource = document.getElementById("backgroundSource");
+let displayFlag = 0;
+
+window.onresize = function () {
+    let width = document.documentElement.clientWidth;
+    if (width < 480) {
+        hideBackground();
     }
 }
+
+background.onclick = function () {
+    displayFlag === 0 ? showBackground() : hideBackground();
+}
+
+document.body.onload = function () {
+    if (document.documentElement.clientWidth > 480) {
+        background.style.opacity = "1";
+    }
+}
+
+function showBackground() {
+    header.style.opacity = "0";
+    footer.style.opacity = "0";
+    backgroundSource.style.opacity = "1";
+    for (let i = 0; i < card.length; i++) {
+        card[i].style.opacity = "0";
+    }
+    setTimeout(() => {
+        main.style.zIndex = "20";
+        background.style.zIndex = "20";
+    }, 300);
+
+    background.style.filter = "blur(0)";
+    displayFlag = 1;
+}
+
+function hideBackground() {
+    main.style.zIndex = "2";
+    background.style.zIndex = "2";
+    header.style.opacity = "1";
+    footer.style.opacity = "1";
+    backgroundSource.style.opacity = "0";
+    for (let i = 0; i < card.length; i++) {
+        card[i].style.opacity = 1;
+    }
+    background.style.filter = "blur(15px)";
+    displayFlag = 0;
+}
+
 
 /*
 // div 随鼠标移动代码
