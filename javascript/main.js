f20d109f086eb782c9c717e8f550bc34d6af3d70
Lifeni <liangfengning@foxmail.com>
2019-09-29 21:29:36 +0800

    添加作品界面

--- javascript/main.js
+++ javascript/main.js
@@ -3,18 +3,28 @@
 let navButton = document.querySelectorAll(".nav-button"),
     content = document.querySelectorAll(".content"),
     directory = document.querySelector(".directory");
-let getFlag = 0;
+
+let getNotesFlag = 0,
+    getWorksFlag = 0;
 
 document.addEventListener("DOMContentLoaded", function () {
     navButton[0].click();
 })
 
 navButton[0].onclick = function () {
-    if (!getFlag) {
+    if (!getNotesFlag) {
         getNotes();
-        getFlag = 1;
+        getNotesFlag = 1;
     }
     showContent(0);
+};
+
+navButton[1].onclick = function () {
+    if (!getWorksFlag) {
+        getWorks();
+        getWorksFlag = 1;
+    }
+    showContent(1);
 };
 
 function showContent(x) {
@@ -35,15 +45,16 @@ function getNotes() {
     getJson.onreadystatechange = function () {
         if (this.readyState === 4 && this.status === 200) {
             let notebookJson = JSON.parse(this.responseText);
+
             for (let i = 0; i < notebookJson.length; i++) {
+                if (notebookJson[i].type == "file") {
+                    continue;
+                }
+
                 let newNotebook = document.createElement("div");
                 newNotebook.className = "notebook";
                 newNotebook.setAttribute("id", notebookJson[i].name);
-
-                if (notebookJson[i].type == "file") {
-                    newNotebook.style.display = "none";
-                    continue;
-                }
+                content[0].appendChild(newNotebook);
 
                 let newLink = document.createElement("a");
                 newLink.className = "directory-link";
@@ -64,6 +75,7 @@ function getNotes() {
                 getJson2.onreadystatechange = function () {
                     if (this.readyState === 4 && this.status === 200) {
                         let noteJson = JSON.parse(this.responseText);
+
                         for (let j = 0; j < noteJson.length; j++) {
                             let newItem = document.createElement("li");
                             newItem.className = "note-item";
@@ -72,23 +84,81 @@ function getNotes() {
 
                             let newLink = document.createElement("a");
                             newLink.className = "note-link";
+                            newLink.href = noteJson[j].html_url;
                             newLink.appendChild(newItem);
-                            newLink.setAttribute("href", noteJson[j].html_url);
+
                             newList.appendChild(newLink);
                         }
                     }
                 }
-                getJson2.open("GET", notebookJson[i].url, true);
-                getJson2.send();
 
-                content[0].appendChild(newNotebook);
-                if (i != notebookJson.length - 1) {
-                    let newHr = document.createElement("hr");
-                    content[0].appendChild(newHr);
-                }
+                getJson2.open("GET", notebookJson[i].url + jsonToken("&"), true);
+                getJson2.send();
             }
         }
     }
-    getJson.open("GET", "https://api.github.com/repos/Lifeni/lifeni-notes/contents", true);
+
+    getJson.open("GET", "https://api.github.com/repos/Lifeni/lifeni-notes/contents" + jsonToken("?"), true);
     getJson.send();
+}
+
+function getWorks() {
+    let getJson = new XMLHttpRequest();
+    getJson.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
+            let worksJson = JSON.parse(this.responseText);
+
+            for (let i = 0; i < worksJson.length; i++) {
+                if (worksJson[i].name.startsWith("lifeni")) {
+                    continue;
+                }
+
+                let newShowcase = document.createElement("div");
+                newShowcase.className = "showcase";
+                newShowcase.setAttribute("id", worksJson[i].name);
+                content[1].appendChild(newShowcase);
+
+                let newLink = document.createElement("a");
+                newLink.className = "showcase-link";
+                newLink.href = worksJson[i].html_url;
+                newShowcase.appendChild(newLink);
+
+                let newTitle = document.createElement("div");
+                newTitle.className = "showcase-title";
+                newTitle.innerText = worksJson[i].name;
+                newLink.appendChild(newTitle);
+
+                let newDescription = document.createElement("div");
+                newDescription.className = "showcase-description";
+                newDescription.innerText = worksJson[i].description;
+                newLink.appendChild(newDescription);
+            }
+
+            let newShowcase = document.createElement("div");
+            newShowcase.className = "showcase";
+            content[1].appendChild(newShowcase);
+
+            let newLink = document.createElement("a");
+            newLink.className = "showcase-link";
+            newLink.href = "https://github.com/Lifeni";
+            newShowcase.appendChild(newLink);
+
+            let newTitle = document.createElement("div");
+            newTitle.className = "showcase-title";
+            newTitle.innerText = "还有什么?";
+            newLink.appendChild(newTitle);
+
+            let newDescription = document.createElement("div");
+            newDescription.className = "showcase-description";
+            newDescription.innerText = "去 Github 看看";
+            newLink.appendChild(newDescription);
+        }
+    }
+
+    getJson.open("GET", "https://api.github.com/users/Lifeni/repos" + jsonToken("?"), true);
+    getJson.send();
+}
+
+function jsonToken(sign) {
+    return "";
 }
